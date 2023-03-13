82c3ca7b29c30e419c5c3439f8c74cd69de53a70
ManiDesk <sterlowebsite@gmail.com>
2023-03-14 02:12:11 +0530

    pwa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "animate.css/animate.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,4 +18,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorkerRegistration.register();
